e57be999b119bf54648b9419748c28393a915928
Eric Klavins <klavins@uw.edu>
2020-01-14 16:25:37 -0800

    extracted components, improved performance

--- slider.js
+++ slider.js
@@ -1,50 +1,100 @@
 'use strict';
 
-// https://www.w3schools.com/howto/howto_js_fullscreen.asp
-// document.documentElement.webkitRequestFullscreen();
-
 const e = React.createElement;
 const converter = new showdown.Converter();
 
-function parse(text) {
-  let lines = text.split("\n");
-  let sections = [];
-  let i = 0;
-  let section = "";
-  while (i < lines.length) {
-    if (i + 2 < lines.length && lines[i + 1] == "===") {
-      if (section != "") {
-        sections.push(section);
-      }
-      section = "";
-    }
-    section += lines[i] + "\n";
+class Thumbnail extends React.PureComponent {
 
-    i++;
+  constructor(props) {
+    super(props);
+    this.go = this.go.bind(this);
   }
-  sections.push(section);
-  return sections;
+
+  go() {
+    if (typeof this.props.go === 'function') {
+      this.props.go(this.props.id);
+    }
+  }
+
+  render() {
+    let classes = "title";
+    if (this.props.active) {
+      classes += " active-title";
+    }
+    return React.createElement(
+      'div',
+      { onClick: this.go,
+        className: classes },
+      this.props.title
+    );
+  }
+
+}
+
+class Slide extends React.PureComponent {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let classes = "markdown-body slide";
+    if (this.props.active) {
+      classes += " active-slide";
+    }
+    return React.createElement('div', { className: classes,
+      dangerouslySetInnerHTML: { __html: converter.makeHtml(this.props.content) } });
+  }
+
+}
+
+class Deck extends React.PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.switch = this.switch.bind(this);
+  }
+
+  switch() {
+    if (typeof this.props.switch === 'function') {
+      this.props.switch(this.props.id);
+    }
+  }
+
+  render() {
+    let classes = "deck";
+    if (this.props.active) {
+      classes += " active-deck";
+    }
+    return React.createElement(
+      'div',
+      { onClick: this.switch,
+        className: classes },
+      React.createElement(
+        'span',
+        null,
+        this.props.id + 1,
+        ': ',
+        this.props.title
+      )
+    );
+  }
+
 }
 
 class Slider extends React.Component {
 
   constructor(props) {
+
     super(props);
 
     let slide = parseInt(Cookies.get("slide"));
-    if (!slide) {
-      slide = 0;
-    }
-
     let deck = parseInt(Cookies.get("deck"));
-    if (!deck) {
-      deck = 0;
-    }
-
     let sb = Cookies.get("sidebar");
-    if (!sb) {
-      sb = "decks";
-    }
+
+    slide = slide ? slide : 0;
+    deck = deck ? deck : 0;
+    sb = sb ? sb : "decks";
 
     this.state = {
       error: null,
@@ -55,12 +105,14 @@ class Slider extends React.Component {
       fullscreen: false,
       sidebar: sb
     };
+
     this.forward = this.forward.bind(this);
     this.reverse = this.reverse.bind(this);
     this.go = this.go.bind(this);
     this.switch_deck = this.switch_deck.bind(this);
     this.fullscreen = this.fullscreen.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.scroll_animation = null;
   }
 
   componentDidMount() {
@@ -68,7 +120,7 @@ class Slider extends React.Component {
       this.config = config;
       return fetch(config.slide_decks[this.state.deck].path);
     }).then(res => res.text()).then(result => {
-      let slides = parse(result);
+      let slides = this.parse(result);
       let titles = slides.map(s => s.split("===")[0]);
       this.setState({
         isLoaded: true,
@@ -81,6 +133,25 @@ class Slider extends React.Component {
         error
       });
     });
+  }
+
+  parse(text) {
+    let lines = text.split("\n");
+    let sections = [];
+    let i = 0;
+    let section = "";
+    while (i < lines.length) {
+      if (i + 2 < lines.length && lines[i + 1] == "===") {
+        if (section != "") {
+          sections.push(section);
+        }
+        section = "";
+      }
+      section += lines[i] + "\n";
+      i++;
+    }
+    sections.push(section);
+    return sections;
   }
 
   forward() {
@@ -111,161 +182,99 @@ class Slider extends React.Component {
   }
 
   fullscreen() {
-    if (this.state.fullscreen) {
-      document.webkitExitFullscreen();
-    } else {
-      document.documentElement.webkitRequestFullscreen();
-    }
+    this.state.fullscreen ? document.webkitExitFullscreen() : document.documentElement.webkitRequestFullscreen();
     this.setState({ fullscreen: !this.state.fullscreen });
   }
 
   handleKeyDown(event) {
-    if (event.key == "ArrowRight") {
-      this.forward();
-    } else if (event.key == "ArrowLeft") {
-      this.reverse();
+    if (this.scroll_animation == null) {
+      if (event.key == "ArrowRight") {
+        this.forward();
+      } else if (event.key == "ArrowLeft") {
+        this.reverse();
+      }
     }
   }
 
-  slide_divs(slides) {
-    return slides.flatMap((s, i) => {
-      let classes = "markdown-body slide";
-      if (this.state.slide == i) {
-        classes += " active-slide";
-      }
-      return React.createElement("div", { key: i,
-        className: classes,
-        dangerouslySetInnerHTML: { __html: converter.makeHtml(s) } });
-    });
-  }
-
-  title_divs(slides) {
-    let titles = slides.map(s => s.split("===")[0]);
-    return titles.flatMap((t, i) => {
-      let classes = "title";
-      if (this.state.slide == i) {
-        classes += " active-title";
-      }
-      return React.createElement(
-        "div",
-        { key: i,
-          onClick: () => {
-            this.go(i);
-          },
-          className: classes },
-        t
-      );
-    });
-  }
-
-  deck_divs() {
-    return this.config.slide_decks.flatMap((d, i) => {
-      let classes = "deck";
-      if (this.state.deck == i) {
-        classes += " active-deck";
-      }
-      return React.createElement(
-        "div",
-        { key: i,
-          onClick: () => {
-            this.switch_deck(i);
-          },
-          className: classes },
-        React.createElement(
-          "span",
-          null,
-          i + 1,
-          ": ",
-          d.title
-        )
-      );
-    });
-  }
-
-  buttons() {
+  buttons(props) {
     return React.createElement(
-      "div",
+      'div',
       null,
       React.createElement(
-        "button",
-        { id: "forward-button",
+        'button',
+        { id: 'forward-button',
           onClick: this.forward,
           disabled: this.state.slide == this.state.slides.length - 1 },
-        "\u25B6"
+        '\u25B6'
       ),
       React.createElement(
-        "button",
-        { id: "reverse-button",
+        'button',
+        { id: 'reverse-button',
           onClick: this.reverse,
           disabled: this.state.slide == 0 },
-        "\u25C0"
+        '\u25C0'
       ),
       React.createElement(
-        "button",
-        { id: "expand-button",
+        'button',
+        { id: 'expand-button',
           onClick: this.fullscreen },
-        "\u25F3"
+        '\u25F3'
       ),
       React.createElement(
-        "button",
-        { id: "sidebar-button",
+        'button',
+        { id: 'sidebar-button',
           onClick: () => {
-            if (this.state.sidebar == "decks") {
-              this.setState({ sidebar: "slides" });
-              Cookies.set("sidebar", "slides");
-            } else {
-              this.setState({ sidebar: "decks" });
-              Cookies.set("sidebar", "decks");
-            }
+            let sb = this.state.sidebar == "decks" ? "slides" : "decks";
+            this.setState({ sidebar: sb });
+            Cookies.set("sidebar", sb);
           } },
         this.state.sidebar != "decks" ? "Decks" : "Slides"
       )
     );
   }
 
-  make_sidebar_title() {}
-
   render() {
-    const { error, isLoaded, slides } = this.state;
+    const { error, isLoaded, slides, titles } = this.state;
     if (error) {
       return React.createElement(
-        "div",
+        'div',
         null,
-        "Error: ",
+        'Error: ',
         error.message
       );
     } else if (!isLoaded) {
       return React.createElement(
-        "div",
+        'div',
         null,
-        "Loading..."
+        'Loading...'
       );
     } else {
-      let sidebar = "";
-      let sidebar_title = this.make_sidebar_title();
-      if (this.state.sidebar == "slides") {
-        sidebar = React.createElement(
-          "div",
-          { className: "sidebar" },
-          sidebar_title,
-          this.title_divs(slides)
-        );
-      } else {
-        sidebar = React.createElement(
-          "div",
-          { className: "sidebar" },
-          sidebar_title,
-          this.deck_divs()
-        );
-      }
       return React.createElement(
-        "div",
-        { tabIndex: "0", onKeyDown: this.handleKeyDown, className: "slider-container" },
-        sidebar,
+        'div',
+        { tabIndex: '0', onKeyDown: this.handleKeyDown, className: 'slider-container' },
         React.createElement(
-          "div",
-          { className: "slides-container" },
-          this.slide_divs(slides),
+          'div',
+          { className: 'sidebar' },
+          React.createElement(
+            'div',
+            { style: { display: this.state.sidebar == "slides" ? 'block' : 'none' } },
+            titles.flatMap((t, i) => React.createElement(Thumbnail, { key: i, id: i, title: t,
+              active: this.state.slide == i,
+              go: this.go }))
+          ),
+          React.createElement(
+            'div',
+            { style: { display: this.state.sidebar == "decks" ? 'block' : 'none' } },
+            this.config.slide_decks.flatMap((d, i) => React.createElement(Deck, { key: i, id: i, title: d.title,
+              active: this.state.deck == i,
+              'switch': this.switch_deck }))
+          )
+        ),
+        React.createElement(
+          'div',
+          { className: 'slides-container' },
+          slides.flatMap((s, i) => React.createElement(Slide, { key: i, id: i, content: s, 'switch': this.switch_deck,
+            active: this.state.slide == i })),
           this.buttons()
         )
       );
@@ -281,23 +290,30 @@ class Slider extends React.Component {
     if (this.state.sidebar == "slides") {
 
       let sidebar = document.querySelectorAll('.sidebar')[0];
-      let active_title = document.querySelectorAll('.active-title')[0];
+      let active_thumb = document.querySelectorAll('.active-title')[0];
       let initial = sidebar.scrollTop;
-      let target = Math.max(active_title.offsetTop - sidebar.clientHeight / 2, 0);
+      let target = Math.max(active_thumb.offsetTop - sidebar.clientHeight / 2, 0);
       let current = initial;
       let t = 0,
           T = 1 * Math.abs(target - initial);
+      const DT = 5;
 
-      var scroll_animation = setInterval(() => {
+      if (this.scroll_animation != null) {
+        clearInterval(this.scroll_animation);
+        this.scroll_animation = null;
+      }
+
+      this.scroll_animation = setInterval(() => {
         let p = t / T;
         current = p * target + (1 - p) * initial;
         sidebar.scrollTo(0, current);
-        t += 1;
+        t += DT;
         if (t >= T) {
-          clearInterval(scroll_animation);
+          clearInterval(this.scroll_animation);
+          this.scroll_animation = null;
           sidebar.scrollTo(0, target);
         }
-      }, 1);
+      }, DT);
     }
   }
 
